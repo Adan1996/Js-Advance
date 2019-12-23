46e32e2fce00ecea42784494901541f6774aa308
Adan1996 <syahdanmasyhuri@gmail.com>
2019-12-23 19:34:45 +0700

    mengenal Object.create()

--- script.js
+++ script.js
@@ -17,44 +17,51 @@
 //     }
 // }
 
-// Function declaration
-// function Mahasiswa(nama, energi) {
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
-
-//     mahasiswa.makan = function (porsi) {
-//         this.energi += porsi;
-//         console.log(`Hallo ${this.nama}, selamat makan!`);
-//     }
-
-//     mahasiswa.main = function (jam) {
-//         this.energi -= jam;
-//         console.log(`Halo ${this.nama}, selamat bermain!`);
-//     }
-
-//     return mahasiswa;
-// }
-
-// let syahdan = Mahasiswa('Syahdan', 10);
-// let dimas = Mahasiswa('Dimas', 20);
-
-// Constructor function
-// Menggunakan keyword new
-function Mahasiswa(nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
-
-    this.makan = function (porsi) {
+const methodMahasiswa = {
+    makan: function (porsi) {
         this.energi += porsi;
-        console.log(`Halo ${this.nama}, selamat makan!`);
-    }
+        console.log(`Hallo ${this.nama}, selamat makan!`);
+    },
 
-    this.main = function (jam) {
+    main: function (jam) {
         this.energi -= jam;
-        console.log(`Halo ${this.nama}, selamat bermain`);
+        console.log(`Halo ${this.nama}, selamat bermain!`);
+    },
+
+    tidur: function (jam) {
+        this.energi += jam * 2;
+        console.log(`Halo ${this.nama}, selamat tidur!`);
     }
 }
 
-let syahdan = new Mahasiswa('Syahdan Masyhuri', 10);
+// Function declaration
+function Mahasiswa(nama, energi) {
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+
+    return mahasiswa;
+}
+
+let syahdan = Mahasiswa('Syahdan', 10);
+let dimas = Mahasiswa('Dimas', 20);
+
+// Constructor function
+// Menggunakan keyword new
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+
+//     this.makan = function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan!`);
+//     }
+
+//     this.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain`);
+//     }
+// }
+
+// let syahdan = new Mahasiswa('Syahdan Masyhuri', 10);
 // Object.create()
