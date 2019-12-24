c3986015e9f9bfb785563053c83804de52389236
Adan1996 <syahdanmasyhuri@gmail.com>
2019-12-24 12:46:13 +0700

    prototype

--- script.js
+++ script.js
@@ -17,34 +17,34 @@
 //     }
 // }
 
-const methodMahasiswa = {
-    makan: function (porsi) {
-        this.energi += porsi;
-        console.log(`Hallo ${this.nama}, selamat makan!`);
-    },
+// const methodMahasiswa = {
+//     makan: function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Hallo ${this.nama}, selamat makan!`);
+//     },
 
-    main: function (jam) {
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, selamat bermain!`);
-    },
+//     main: function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain!`);
+//     },
 
-    tidur: function (jam) {
-        this.energi += jam * 2;
-        console.log(`Halo ${this.nama}, selamat tidur!`);
-    }
-}
+//     tidur: function (jam) {
+//         this.energi += jam * 2;
+//         console.log(`Halo ${this.nama}, selamat tidur!`);
+//     }
+// }
 
-// Function declaration
-function Mahasiswa(nama, energi) {
-    let mahasiswa = Object.create(methodMahasiswa);
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
+// // Function declaration
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
 
-    return mahasiswa;
-}
+//     return mahasiswa;
+// }
 
-let syahdan = Mahasiswa('Syahdan', 10);
-let dimas = Mahasiswa('Dimas', 20);
+// let syahdan = Mahasiswa('Syahdan', 10);
+// let dimas = Mahasiswa('Dimas', 20);
 
 // Constructor function
 // Menggunakan keyword new
@@ -65,3 +65,54 @@ let dimas = Mahasiswa('Dimas', 20);
 
 // let syahdan = new Mahasiswa('Syahdan Masyhuri', 10);
 // Object.create()
+
+// Object dengan menggunakan Prototype
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+// }
+
+// Mahasiswa.prototype.makan = function (porsi) {
+//     this.energi += porsi;
+//     return `Halo ${this.nama}, selamat makan. Ini ada makanan sebanyak ${porsi} porsi`;
+// }
+
+// Mahasiswa.prototype.main = function (jam) {
+//     this.energi -= jam;
+//     return `Halo ${this.nama}, selamat bermain!`;
+// }
+
+// Mahasiswa.prototype.tidur = function (jam) {
+//     this.energi += jam * 2;
+//     return `Halo ${this.nama}, anda sudah tidur sebanyak ${jam} jam`;
+// }
+
+// let syahdan = new Mahasiswa('Syahdan', 10);
+// let dimas = new Mahasiswa('Dimas', 20);
+
+// Object dengan menggunakan Class
+
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+
+    makan(porsi) {
+        this.energi += porsi;
+        return `Halo ${this.nama}, selamat makan. Ini ada makanan sebanyak ${porsi} porsi`;
+    }
+
+    main(jam) {
+        this.energi -= jam;
+        return `Halo ${this.nama}, selamat bermain!`;
+    }
+
+    tidur(jam) {
+        this.energi += jam * 2;
+        return `Halo ${this.nama}, anda sudah tidur sebanyak ${jam} jam`;
+    }
+}
+
+let syahdan = new Mahasiswa('Syahdan', 50);
+let dimas = new Mahasiswa('Dimas', 30);
