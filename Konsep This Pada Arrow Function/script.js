97f1884407e516824ee775457f31df857ddce4e9
Adan1996 <syahdanmasyhuri@gmail.com>
2020-01-15 01:55:07 +0700

    konsep this pada arrow function dan menambahkan style.css

--- Konsep This Pada Arrow Function/script.js
+++ Konsep This Pada Arrow Function/script.js
@@ -4,16 +4,53 @@
 // let Mahasiswa = function () {
 //     this.nama = 'Syahdan';
 //     this.umur = 24;
-//     this.sayHello = function () {
+//     this.sayHello = () => {
 //         console.log(`Halo, nama saya ${this.nama}, saya berumur ${this.umur}`);
 //     }
 // }
 
 // const syahdan = new Mahasiswa();
 
+// Contoh 2
+// menggunakan object literal
+// const mhs1 = {
+//     nama: 'Syahdan',
+//     umur: 24,
+//     sayHello: () => {
+//         // console.log(`Halo, nama saya ${this.nama}, saya berumur ${this.umur}`);
+//         console.log(this);
+//     }
+// }
 
+// Contoh 3
+// let Mahasiswa = function () {
+//     this.nama = 'Syahdan';
+//     this.umur = 24;
+//     this.sayHello = () => {
+//         console.log(`Halo, nama saya ${this.nama}, saya berumur ${this.umur}`);
+//     }
 
+//     setInterval(() => {
+//         console.log(this.umur++);
+//     }, 500);
+// }
 
+// const syahdan = new Mahasiswa();
+
+const box = document.querySelector('.box');
+box.addEventListener('click', function () {
+    let satu = 'size';
+    let dua = 'caption';
+
+    if (this.classList.contains(satu)) {
+        [satu, dua] = [dua, satu];
+    }
+
+    this.classList.toggle(satu);
+    setTimeout(() => {
+        this.classList.toggle(dua);
+    }, 600);
+});
 
 
 
